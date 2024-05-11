ff2b4ad3d20c6877a388af2f12b7b21af31a8689
Martin Klacan <martinklacan111@gmail.com>
2024-05-11 12:03:13 +0200

    added saving of masks

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
+from ultralytics import settings
+import os
 import cv2
 import numpy as np
+import pathlib
 from pathlib import Path
 
 # TO DO
   # - disable irrelevant coco classes
-  # - create and save masks
 
 def createMask(result, start_point, end_point):
   orig_size = result.orig_shape
@@ -18,25 +20,47 @@ def createMask(result, start_point, end_point):
   color = (255, 255, 255)
   thickness = -1 # will fill the entire shape
 
+  # add white rectangle to the black image
   img = cv2.rectangle(img, start_point, end_point, color, thickness)
 
   return img
 
-# def saveMasks(masks):
-  # TO DO
-    # - get path that the model uses to save results
-    # - create a folder there for each image
-    # - save corresponiding masks in the corresponding folder
+def getPath():
+  runs_dir = settings['runs_dir']
+  temp_path = runs_dir + '\\detect\\'
+
+  # find the latest modified/created directory
+  newest_dir = max(pathlib.Path(temp_path).glob('*/'), key=os.path.getmtime)
+  return newest_dir
+
+def saveMasks(masks, img_id, path):
+  # make new directory for image masks
+  parent_directory = path
+  new_directory = 'masks-room' + str(img_id)
+  final_path = os.path.join(parent_directory, new_directory)
+  os.mkdir(final_path)
+  
+  # save masks into the new directory
+  i = 0
+  for mask in masks:
+    i += 1
+    name = 'mask' + str(i) + '.png'
+    img_path = os.path.join(final_path, name)
+    cv2.imwrite(img_path, mask)
 
 def getImages():
 
+  # REPLACE WITH :
+  # source = 'path/to/dir'
+  # results = model(source, stream=True)  # generator of Results objects
+
   # add all input images to a list
-  tempList = []
+  temp_list = []
   folder_dir = 'input-images'
   images = Path(folder_dir).glob('*.jpg')
   for image in images:
-    tempList.append(image)
-  return tempList
+    temp_list.append(image)
+  return temp_list
 
 def main():
 
@@ -44,10 +68,15 @@ def main():
 
   source = getImages()
 
-  results = model(source)
-  print(results)
+  img_id = 0
+
+  results = model(source, save=True)
+  
+  path = getPath()
 
   for r in results:
+    img_id += 1
+
     # get coordinates of all bounding boxes
     boxes = r.boxes.numpy()
     xyxys = boxes.xyxy
@@ -60,8 +89,7 @@ def main():
       mask = createMask(r, start_point, end_point)
       masks.append(mask)
 
-    # print(xyxys)  # print the Boxes object containing the detection bounding boxes
-    saveMasks(masks)
+    saveMasks(masks, img_id, path)
 
 if __name__ == '__main__':
   main()
